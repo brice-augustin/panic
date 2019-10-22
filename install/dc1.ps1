e2522a436f046d57d162e6b0ebe62ccac4f8f225
Brice <brice.augustin@u-pec.fr>
2019-10-22 15:04:12 +0200

    Désactiver l'update des certificats racine (écran noir + cpu 100% pendant qq minutes si pas d'accès internet)

--- install/dc1.ps1
+++ install/dc1.ps1
@@ -3,5 +3,9 @@ Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 0
 
+New-Item -Path 'HKLM:\Software\Policies\Microsoft\SystemCertificates' -Name 'AuthRoot' –Force
+
+Set-ItemProperty 'HKLM:\Software\Policies\Microsoft\SystemCertificates\AuthRoot' -Name 'DisableRootAutoUpdate' -Value 1
+
 # En version anglaise : 'Remote Desktop' ...
 Enable-NetFirewallRule -DisplayGroup 'Bureau à distance'
