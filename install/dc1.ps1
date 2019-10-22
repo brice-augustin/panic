d7744230a88ac0daa4016809de6706ddf0d0b12a
Brice <brice.augustin@u-pec.fr>
2019-10-22 13:57:10 +0200

    encodage windows pour les accents ('Bureau à distance')

--- install/dc1.ps1
+++ install/dc1.ps1
@@ -1,4 +1,7 @@
 # https://theitbros.com/how-to-remotely-enable-remote-desktop-using-powershell/
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name 'fDenyTSConnections' -Value 0
+
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 0
+
 # En version anglaise : 'Remote Desktop' ...
-Enable-NetFirewallRule -DisplayGroup 'Bureau à distance'
+Enable-NetFirewallRule -DisplayGroup 'Bureau � distance'
