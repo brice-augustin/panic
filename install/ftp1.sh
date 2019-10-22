da03b2d6e3a31213b9eec4cf789d4f5e3bef3e89
Brice <brice.augustin@u-pec.fr>
2019-10-22 18:21:53 +0200

    renommage

--- install/ftp1.sh
+++ install/ftp1.sh
@@ -8,3 +8,9 @@ systemctl start vsftpd
 
 # Pourquoi ?!
 arp -d $FTP1_IP &>> $LOGFILE
+
+hostnamectl set-hostname ftp1
+sed -i "s/^127.0.1.1\s.*/127.0.1.1 ftp1/" /etc/hosts
+
+# dernière commande, ça ferme la session
+skill -KILL -u etudiant
