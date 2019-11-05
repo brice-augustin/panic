835117428d6cb7508f28dbe49c3ddcdfde53d518
Brice <brice.augustin@u-pec.fr>
2019-11-05 13:25:30 +0100

    Sur le réseau Adista 8.8.8.8 est dorénavant accessible

--- incidents/incident6.sh
+++ incidents/incident6.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed -i 's/nameserver .*/nameserver 8.8.8.8/' /etc/resolv.conf
+# Sur le réseau Adista 8.8.8.8 est dorénavant accessible !
+sed -i 's/nameserver .*/nameserver 172.16.30.42/' /etc/resolv.conf
